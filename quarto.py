5f789551eea6f3ba00f996a0f05b202152b1c11e
takasyo <dummy.address.0128@gmail.com>
2020-01-25 01:53:28 +0900

    Add show quarto position to draw_map()

--- quarto.py
+++ quarto.py
@@ -1,19 +1,19 @@
 import random
 
 mapp="""
-　　｜Ａ　｜Ｂ　｜Ｃ　｜Ｄ　｜
+{1[8]}　｜Ａ{1[4]}｜Ｂ{1[5]}｜Ｃ{1[6]}｜Ｄ{1[7]}｜{1[9]}
 ＿＿丄＿＿丄＿＿丄＿＿丄＿＿」
 １　｜{0[0][0]}{0[0][1]}｜{0[1][0]}{0[1][1]}｜{0[2][0]}{0[2][1]}｜{0[3][0]}{0[3][1]}｜
-　　｜{0[0][2]}{0[0][3]}｜{0[1][2]}{0[1][3]}｜{0[2][2]}{0[2][3]}｜{0[3][2]}{0[3][3]}｜
+{1[0]}　｜{0[0][2]}{0[0][3]}｜{0[1][2]}{0[1][3]}｜{0[2][2]}{0[2][3]}｜{0[3][2]}{0[3][3]}｜
 ＿＿丄＿＿丄＿＿丄＿＿丄＿＿」
 ２　｜{0[4][0]}{0[4][1]}｜{0[5][0]}{0[5][1]}｜{0[6][0]}{0[6][1]}｜{0[7][0]}{0[7][1]}｜
-　　｜{0[4][2]}{0[4][3]}｜{0[5][2]}{0[5][3]}｜{0[6][2]}{0[6][3]}｜{0[7][2]}{0[7][3]}｜
+{1[1]}　｜{0[4][2]}{0[4][3]}｜{0[5][2]}{0[5][3]}｜{0[6][2]}{0[6][3]}｜{0[7][2]}{0[7][3]}｜
 ＿＿丄＿＿丄＿＿丄＿＿丄＿＿」
 ３　｜{0[8][0]}{0[8][1]}｜{0[9][0]}{0[9][1]}｜{0[10][0]}{0[10][1]}｜{0[11][0]}{0[11][1]}｜
-　　｜{0[8][2]}{0[8][3]}｜{0[9][2]}{0[9][3]}｜{0[10][2]}{0[10][3]}｜{0[11][2]}{0[11][3]}｜
+{1[2]}　｜{0[8][2]}{0[8][3]}｜{0[9][2]}{0[9][3]}｜{0[10][2]}{0[10][3]}｜{0[11][2]}{0[11][3]}｜
 ＿＿丄＿＿丄＿＿丄＿＿丄＿＿」
 ４　｜{0[12][0]}{0[12][1]}｜{0[13][0]}{0[13][1]}｜{0[14][0]}{0[14][1]}｜{0[15][0]}{0[15][1]}｜
-　　｜{0[12][2]}{0[12][3]}｜{0[13][2]}{0[13][3]}｜{0[14][2]}{0[14][3]}｜{0[15][2]}{0[15][3]}｜
+{1[3]}　｜{0[12][2]}{0[12][3]}｜{0[13][2]}{0[13][3]}｜{0[14][2]}{0[14][3]}｜{0[15][2]}{0[15][3]}｜
 ＿＿丄＿＿丄＿＿丄＿＿丄＿＿」
 """
 search=[
@@ -22,19 +22,28 @@ search=[
     [0,5,10,15], [3,6,9,12]
     ]
 maplist = [''] * 16
-mapstr = [["　"] * 4 for i in range(16)]
+mapstr = [['　'] * 4 for i in range(16)]
 piece = [format(i, '04b') for i in range(16)] #0000~1111
 piecestr = [['黒', '白'], ['高', '低'], ['円', '角'], ['有', '無']]
+quarto_mark = ['　'] * len(search)
 
-def draw_map():
-    '''マップを描写します'''
+def draw_map(search_index=None, str_index_list=None):
+    '''
+    マップを描写します
+    search_index = 0 ~ len(search)
+    str_index_list = 0 ~ 3
+    '''
 
     for i in range(len(maplist)):
         for j in range(4):
             if maplist[i] != '':
-                mapstr[i][j]=piecestr[j][int(maplist[i][j])]
+                mapstr[i][j] = piecestr[j][int(maplist[i][j])]
 
-    print(mapp.format(mapstr))
+    if type(search_index) == int:
+        tmp = maplist[search[search_index][0]][str_index_list]
+        quarto_mark[search_index] = piecestr[str_index_list][int(tmp)]
+    
+    print(mapp.format(mapstr, quarto_mark))
 
 def put_player(choiced):
     '''playerがコマを置きます'''
@@ -130,19 +139,19 @@ def finish():
     
     for i in range(len(search)): # 0~8
         tmp = [1] * 4
-        empty = True
+        empty = False
         for search_index in search[i]: #ex)[0, 4, 8, 12]
             if maplist[search_index] == '':
-                empty = False
+                empty = True
                 break
 
-            for match_index in [x for x, y in enumerate(tmp) if y == 1]: # tmpが1のindexだけ調べる
-                if maplist[search[i][0]][match_index] != maplist[search_index][match_index]:
-                    tmp[match_index] = 0
+            for str_index in [x for x, y in enumerate(tmp) if y == 1]: # tmpが1のindexだけ調べる
+                if maplist[search[i][0]][str_index] != maplist[search_index][str_index]:
+                    tmp[str_index] = 0
             
-        if 1 in tmp and empty: # tmpに1つでも1がある
+        if 1 in tmp and empty is False: # tmpに1つでも1がある
             print("quartoです")   
-            draw_map() 
+            draw_map(i, tmp.index(1)) 
             return True
 
     return False
